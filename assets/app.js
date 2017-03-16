7b1fb779eff238f0fd315fe9a3dab56b638d1c2a
meeshyd <michellemdidier@gmail.com>
2017-03-16 17:58:45 -0500

    cleaned up some bugs in app.js, added media query to style.cs, added footer and credit to the giphy api

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,7 @@
 var topics = [
-"kramer",
-"elaine",
+"cosmo kramer",
+"elaine benes",
 "jerry seinfeld",
-"newman",
 "george costanza",
 ];
 
@@ -11,57 +10,64 @@ var $addGif = $("#add-gif");
 var $gifContainer = $("#gif-container");
 var $button;
 
+//button creates buttons for each item in the topics array. the function also assigns the seinfeld-term attribute used in the displayGifs function to generate related gifs
 function createButtons() {
 	$buttonDiv.empty();
 	for (var i=0; i<topics.length; i++) {
-		$button= $("<button>").addClass("gif-button").text(topics[i]).attr("data-name", topics[i]);
+		$button= $("<button>").addClass("gif-button").text(topics[i]).attr("seinfeld-term", topics[i]);
 		$buttonDiv.append($button);
 	};
 };
 
 createButtons();
 
+// when submit button is clicked, stores input in variable, pushes to array, and calls function that creates buttons
+$addGif.on("click",function(event) {
+	event.preventDefault();
+
+	var userSearch = $("#gif-input").val().trim();
+	
+	topics.push(userSearch);
+	
+	$('#gif-input').val('');
+	
+	createButtons();
+});
+
+//when buttons with class gif-button are clicked, calls function that generates gifs from GIPHY API
+$(document).on("click", ".gif-button", displayGifs);
+
+//this function includes AJAX call to GIPHY API. it takes the Seinfeld term attribute assigned to the clicked button and displays 10 gifs related to that attribute
 function displayGifs() {
 	$gifContainer.empty();
-	var dataName = $(this).attr("data-name")
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+dataName+"&api_key=dc6zaTOxFJmzC&limit=10"
+	var seinfeldTerm = $(this).attr("seinfeld-term")
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+seinfeldTerm+"&api_key=dc6zaTOxFJmzC&limit=10"
 
 	$.ajax({
 		url: queryURL,
 		method: "GET",
 	}).done(function(response) {
-		console.log (response.data)
 		var giphyResults = response.data;
-
+		//for loop to generate the 10 gifs returned from AJAX response data
 		for (i=0; i<giphyResults.length; i++) {
 			var $gifDiv = $("<div>").addClass("gif-div");
 			var $p = $('<p>').text("Rating: " + giphyResults[i].rating);
-			var $gif = $("<img>").addClass("gif").attr("src",giphyResults[i].images.fixed_width_still.url).attr("data-animate",giphyResults[i].images.fixed_width.url).attr("data-still", giphyResults[i].images.fixed_width_still.url).attr("data-state", "still");
+			var $gif = $("<img>").addClass("gif").attr("src",giphyResults[i].images.fixed_width_still.url).attr("gif-animate",giphyResults[i].images.fixed_width.url).attr("gif-still", giphyResults[i].images.fixed_width_still.url).attr("gif-state", "still");
 			$gifDiv.append($p);
-			$gifDiv.append($gif);
+			$gifDiv.prepend($gif);
 			$gifContainer.append($gifDiv);
 		};
+		//this is a click event to pause and play gifs
 		$(".gif").on("click", function() {
-      		var state = $(this).attr("data-state");
+      		var state = $(this).attr("gif-state");
 
       		if (state == "still") {
-        		$(this).attr("src", $(this).attr("data-animate"));
-        		$(this).attr("data-state", "animate");
+        		$(this).attr("src", $(this).attr("gif-animate"));
+        		$(this).attr("gif-state", "animate");
       		}else{
-        		$(this).attr("src", $(this).attr("data-still"));
-        		$(this).attr("data-state", "still");
+        		$(this).attr("src", $(this).attr("gif-still"));
+        		$(this).attr("gif-state", "still");
     		};
 		});
 	});
 };
-
-
-$addGif.on("click",function(event) {
-	event.preventDefault();
-
-	var userSearch = $("#gif-input").val().trim();
-	topics.push(userSearch);
-	createButtons();
-});
-
-$(document).on("click", ".gif-button", displayGifs);
